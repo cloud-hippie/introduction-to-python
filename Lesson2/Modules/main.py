cf7dd22e34c78c193b36c87966551d3528678a5e
Justin Rice <jsrice7391@gmail.com>
2022-03-31 18:25:31 -0400

    Added classes and modules

---
+++ Lesson2/Modules/main.py
@@ -0,0 +1,6 @@
+from request_api import APIClient
+
+
+my_client = APIClient("username", "api_key")
+
+# my.client.get_user_info()
---
+++ Lesson2/Modules/request_api.py
@@ -0,0 +1,6 @@
+
+
+class APIClient:
+    def __init__(self, username, api_key):
+        self.username = username
+        self.API_key = api_key
