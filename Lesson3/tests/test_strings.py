a1b527a7c7c88344dd6a0ad5a342cf87a9578cf4
Justin Rice <jsrice7391@gmail.com>
2022-04-02 22:51:27 -0400

    Added the tests for the functions

--- Lesson3/tests/test_strings.py
+++ Lesson3/tests/test_strings.py
@@ -1,7 +1,41 @@
-from distutils.util import check_environ
-from Lesson3.challenges.strings import count_vowels
+import pytest
+from Lesson3.challenges.strings import count_vowels, is_palindrome, reverse_string
 
+@pytest.mark.parametrize(
+    "name, expected",
+    [
+        ("", 0),
+        ("a", 1),
+        ("ab", 1),
+        ("abcde", 2)
+    ],
+)
+def test_count_vowels(name, expected):
+    assert count_vowels(name) == expected
+    
 
-def test_vowel_count():
-    assert count_vowels("hello") == 2
+@pytest.mark.parametrize(
+    "string, expected",
+    [
+        ("", True),
+        ("a", True),
+        ("ab", False),
+        ("abcde", False),
+        ("abcba", True)
+    ],
+)
+def test_is_palindrome(string, expected):
+    assert is_palindrome(string) == expected
 
+@pytest.mark.parametrize(
+    "string, expected",
+    [
+        ("", ""),
+        ("a", "a"),
+        ("ab", "ba"),
+        ("abcde", "edcba")
+    ],
+)
+def test_reverse_string(string, expected):
+    assert reverse_string(string) == expected
+    
