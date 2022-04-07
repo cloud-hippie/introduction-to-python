9263db29abd455de14c54b6f07930c9c92652d88
Justin Rice <jsrice7391@gmail.com>
2022-04-06 22:08:06 -0400

    Added the click runner and the Config file

--- CLIs/main.py
+++ CLIs/main.py
@@ -13,6 +13,7 @@ def configure(count, name):
     """Simple program that greets NAME for a total of COUNT times."""
     for x in range(count):
         click.echo(f"Hello {name}!")
+    
 
 if __name__ == '__main__':
     cli()
---
+++ CLIs/schemas/config_file.py
@@ -0,0 +1,9 @@
+
+
+class ConfigFile:
+    def __init__(self, file_location=None) -> None:
+        self.file_location = file_location
+
+    def get_value(self, key):
+        pass
+    
