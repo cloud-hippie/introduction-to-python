618c423cbe3fe34de3d5865957688ce3e01445f3
Justin Rice <jsrice7391@gmail.com>
2022-04-19 19:00:40 -0400

    Added the special charaters

--- 06-Review/password-generator/main.py
+++ 06-Review/password-generator/main.py
@@ -1,5 +1,6 @@
 import click
-
+import random
+import string
 
 @click.group()
 def cli():
@@ -7,13 +8,22 @@ def cli():
 
 @cli.command()
 @click.option('--password-length', default=1, help='Number of greetings.')
-def generate(password_length):
+@click.option("--no-numbers", is_flag=True)
+@click.option("--special-chars", is_flag=True,help="Enable special characters")
+def generate(password_length,no_numbers,special_chars):
     """Genetate a string with the given length"""
     output_password = ""
-    click.echo(f"I got a password length of: {password_length}")
-    
+    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    numbers= "0123456789"
+    special_char_choices="!@#$%^&*()"
     for letter in range(0, password_length):
-        output_password += "*"
+        # Add a letter from the alphasbet above
+        output_number = random.randrange(0, 10)
+        if output_number % 2 == 0 and special_chars:
+            master_string = letters + special_char_choices
+            output_password += random.choice(master_string)
+        elif no_numbers == False:
+            output_password += random.choice(numbers)
     
     click.secho(f"I have generated a password: {output_password}", fg="green")
 
